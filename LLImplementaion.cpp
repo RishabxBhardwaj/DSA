d3895762fdaf0acae84afa3e15d265ddde3b9dd0
RishabxBhardwaj <rishabcgc@gmail.com>
2024-10-18 23:26:46 +0530

    Linked list implementation

---
+++ LLImplementaion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Node{
+int data;
+Node* next;
+public:
+node(int val){
+    data=val;
+    next=NULL;
+}
+};
+class List{
+    Node* Head;
+    Node* Tail;
+    public:
+    List(){
+        Head=NULL;
+        Tail=NULL;
+    }
+};
+
+int main(){
+    List ll();
+    return 0;
+}
