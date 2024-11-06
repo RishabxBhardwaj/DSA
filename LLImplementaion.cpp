3278c00bcb441206093cc88091e9ecd1b0d8cc3f
RishabxBhardwaj <rishabcgc@gmail.com>
2024-11-06 06:49:42 +0530

    print ll

--- LLImplementaion.cpp
+++ LLImplementaion.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 class Node{
-int data;
-Node* next;
-public:
-node(int val){
+    public:
+    int data;
+    Node* Next;
+
+    Node(int val){
     data=val;
-    next=NULL;
-}
-};
+    Next=NULL;
+        }
+    };
 class List{
     Node* Head;
     Node* Tail;
@@ -17,9 +18,50 @@ class List{
         Head=NULL;
         Tail=NULL;
     }
+    //For Insertion from Front
+    // void push_front(int val){
+    //     Node* newNode= new Node(val);
+    //     if(Head==NULL){
+    //         Head=Tail=newNode;
+    //     }
+    //     else{
+    //         newNode->Next=Head;
+    //         Head=newNode;
+    //     }
+    // }
+    // For Insertion from Back
+    void push_back(int val){
+        Node* newNode= new Node(val);
+        if(Tail==NULL){
+            Head=Tail=newNode;
+        }
+        else{
+            Tail->Next=newNode;
+            Tail=newNode;
+        }
+    }
+    void print(){
+        Node* temp=Head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->Next;
+        }
+        cout<<"Null";
+    }
+   
 };
 
+
 int main(){
-    List ll();
+  
+    List ll;
+    // ll.push_front(1);
+    // ll.push_front(2);
+    // ll.push_front(3);
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.print();
+ 
     return 0;
 }
